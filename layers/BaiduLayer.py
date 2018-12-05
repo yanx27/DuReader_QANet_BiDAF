ba31a348ae8f661be0d5b72b9d5556e9079c7cd3
benny <35829331+yanx27@users.noreply.github.com>
2018-12-05 16:26:39 +0800

    Baidu+Ours layer function

---
+++ layers/BaiduLayer.py
@@ -0,0 +1,325 @@
+# -*- coding:utf8 -*-
+# ==============================================================================
+# Copyright 2017 Baidu.com, Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""
+This module provides wrappers for variants of RNN in Tensorflow
+"""
+
+import tensorflow as tf
+import tensorflow.contrib as tc
+
+
+def rnn(rnn_type, inputs, length, hidden_size, layer_num=1, dropout_keep_prob=None, concat=True):
+    """
+    Implements (Bi-)LSTM, (Bi-)GRU and (Bi-)RNN
+    Args:
+        rnn_type: the type of rnn
+        inputs: padded inputs into rnn
+        length: the valid length of the inputs
+        hidden_size: the size of hidden units
+        layer_num: multiple rnn layer are stacked if layer_num > 1
+        dropout_keep_prob:
+        concat: When the rnn is bidirectional, the forward outputs and backward outputs are
+                concatenated if this is True, else we add them.
+    Returns:
+        RNN outputs and final state
+    """
+    if not rnn_type.startswith('bi'):
+        cell = get_cell(rnn_type, hidden_size, layer_num, dropout_keep_prob)
+        outputs, states = tf.nn.dynamic_rnn(cell, inputs, sequence_length=length, dtype=tf.float32)
+        if rnn_type.endswith('lstm'):
+            c = [state.c for state in states]
+            h = [state.h for state in states]
+            states = h
+    else:
+        cell_fw = get_cell(rnn_type, hidden_size, layer_num, dropout_keep_prob)
+        cell_bw = get_cell(rnn_type, hidden_size, layer_num, dropout_keep_prob)
+        outputs, states = tf.nn.bidirectional_dynamic_rnn(
+            cell_bw, cell_fw, inputs, sequence_length=length, dtype=tf.float32
+        )
+        states_fw, states_bw = states
+        if rnn_type.endswith('lstm'):
+            c_fw = [state_fw.c for state_fw in states_fw]
+            h_fw = [state_fw.h for state_fw in states_fw]
+            c_bw = [state_bw.c for state_bw in states_bw]
+            h_bw = [state_bw.h for state_bw in states_bw]
+            states_fw, states_bw = h_fw, h_bw
+        if concat:
+            outputs = tf.concat(outputs, 2)
+            states = tf.concat([states_fw, states_bw], 1)
+        else:
+            outputs = outputs[0] + outputs[1]
+            states = states_fw + states_bw
+    return outputs, states
+
+
+def get_cell(rnn_type, hidden_size, layer_num=1, dropout_keep_prob=None):
+    """
+    Gets the RNN Cell
+    Args:
+        rnn_type: 'lstm', 'gru' or 'rnn'
+        hidden_size: The size of hidden units
+        layer_num: MultiRNNCell are used if layer_num > 1
+        dropout_keep_prob: dropout in RNN
+    Returns:
+        An RNN Cell
+    """
+    cells = []
+    for i in range(layer_num):
+        if rnn_type.endswith('lstm'):
+            cell = tc.rnn.LSTMCell(num_units=hidden_size, state_is_tuple=True)
+        elif rnn_type.endswith('gru'):
+            cell = tc.rnn.GRUCell(num_units=hidden_size)
+        elif rnn_type.endswith('rnn'):
+            cell = tc.rnn.BasicRNNCell(num_units=hidden_size)
+        else:
+            raise NotImplementedError('Unsuported rnn type: {}'.format(rnn_type))
+        if dropout_keep_prob is not None:
+            cell = tc.rnn.DropoutWrapper(cell,
+                                         input_keep_prob=dropout_keep_prob,
+                                         output_keep_prob=dropout_keep_prob)
+        cells.append(cell)
+    cells = tc.rnn.MultiRNNCell(cells, state_is_tuple=True)
+    return cells
+
+
+def custom_dynamic_rnn(cell, inputs, inputs_len, initial_state=None):
+    """
+    Implements a dynamic rnn that can store scores in the pointer network,
+    the reason why we implements this is that the raw_rnn or dynamic_rnn function in Tensorflow
+    seem to require the hidden unit and memory unit has the same dimension, and we cannot
+    store the scores directly in the hidden unit.
+    Args:
+        cell: RNN cell
+        inputs: the input sequence to rnn
+        inputs_len: valid length
+        initial_state: initial_state of the cell
+    Returns:
+        outputs and state
+    """
+    batch_size = tf.shape(inputs)[0]
+    max_time = tf.shape(inputs)[1]
+
+    inputs_ta = tf.TensorArray(dtype=tf.float32, size=max_time)
+    inputs_ta = inputs_ta.unstack(tf.transpose(inputs, [1, 0, 2]))
+    emit_ta = tf.TensorArray(dtype=tf.float32, dynamic_size=True, size=0)
+    t0 = tf.constant(0, dtype=tf.int32)
+    if initial_state is not None:
+        s0 = initial_state
+    else:
+        s0 = cell.zero_state(batch_size, dtype=tf.float32)
+    f0 = tf.zeros([batch_size], dtype=tf.bool)
+
+    def loop_fn(t, prev_s, emit_ta, finished):
+        """
+        the loop function of rnn
+        """
+        cur_x = inputs_ta.read(t)
+        scores, cur_state = cell(cur_x, prev_s)
+
+        # copy through
+        scores = tf.where(finished, tf.zeros_like(scores), scores)
+
+        if isinstance(cell, tc.rnn.LSTMCell):
+            cur_c, cur_h = cur_state
+            prev_c, prev_h = prev_s
+            cur_state = tc.rnn.LSTMStateTuple(tf.where(finished, prev_c, cur_c),
+                                              tf.where(finished, prev_h, cur_h))
+        else:
+            cur_state = tf.where(finished, prev_s, cur_state)
+
+        emit_ta = emit_ta.write(t, scores)
+        finished = tf.greater_equal(t + 1, inputs_len)
+        return [t + 1, cur_state, emit_ta, finished]
+
+    _, state, emit_ta, _ = tf.while_loop(
+        cond=lambda _1, _2, _3, finished: tf.logical_not(tf.reduce_all(finished)),
+        body=loop_fn,
+        loop_vars=(t0, s0, emit_ta, f0),
+        parallel_iterations=32,
+        swap_memory=False)
+
+    outputs = tf.transpose(emit_ta.stack(), [1, 0, 2])
+    return outputs, state
+
+
+def attend_pooling(pooling_vectors, ref_vector, hidden_size, scope=None):
+    """
+    Applies attend pooling to a set of vectors according to a reference vector.
+    Args:
+        pooling_vectors: the vectors to pool
+        ref_vector: the reference vector
+        hidden_size: the hidden size for attention function
+        scope: score name
+    Returns:
+        the pooled vector
+    """
+    with tf.variable_scope(scope or 'attend_pooling'):
+        U = tf.tanh(tc.layers.fully_connected(pooling_vectors, num_outputs=hidden_size,
+                                              activation_fn=None, biases_initializer=None)
+                    + tc.layers.fully_connected(tf.expand_dims(ref_vector, 1),
+                                                num_outputs=hidden_size,
+                                                activation_fn=None))
+        logits = tc.layers.fully_connected(U, num_outputs=1, activation_fn=None)
+        scores = tf.nn.softmax(logits, 1)
+        pooled_vector = tf.reduce_sum(pooling_vectors * scores, axis=1)
+    return pooled_vector
+
+
+class PointerNetLSTMCell(tc.rnn.LSTMCell):
+    """
+    Implements the Pointer Network Cell
+    """
+    def __init__(self, num_units, context_to_point):
+        super(PointerNetLSTMCell, self).__init__(num_units, state_is_tuple=True)
+        self.context_to_point = context_to_point
+        self.fc_context = tc.layers.fully_connected(self.context_to_point,
+                                                    num_outputs=self._num_units,
+                                                    activation_fn=None)
+
+    def __call__(self, inputs, state, scope=None):
+        (c_prev, m_prev) = state
+        with tf.variable_scope(scope or type(self).__name__):
+            U = tf.tanh(self.fc_context
+                        + tf.expand_dims(tc.layers.fully_connected(m_prev,
+                                                                   num_outputs=self._num_units,
+                                                                   activation_fn=None),
+                                         1))
+            logits = tc.layers.fully_connected(U, num_outputs=1, activation_fn=None)
+            scores = tf.nn.softmax(logits, 1)
+            attended_context = tf.reduce_sum(self.context_to_point * scores, axis=1)
+            lstm_out, lstm_state = super(PointerNetLSTMCell, self).__call__(attended_context, state)
+        return tf.squeeze(scores, -1), lstm_state
+
+
+class PointerNetDecoder(object):
+    """
+    Implements the Pointer Network
+    """
+    def __init__(self, hidden_size):
+        self.hidden_size = hidden_size
+
+    def decode(self, passage_vectors, question_vectors, init_with_question=True):
+        """
+        Use Pointer Network to compute the probabilities of each position
+        to be start and end of the answer
+        Args:
+            passage_vectors: the encoded passage vectors
+            question_vectors: the encoded question vectors
+            init_with_question: if set to be true,
+                             we will use the question_vectors to init the state of Pointer Network
+        Returns:
+            the probs of evary position to be start and end of the answer
+        """
+        with tf.variable_scope('pn_decoder'):
+            fake_inputs = tf.zeros([tf.shape(passage_vectors)[0], 2, 1])  # not used
+            sequence_len = tf.tile([2], [tf.shape(passage_vectors)[0]])
+            if init_with_question:
+                random_attn_vector = tf.Variable(tf.random_normal([1, self.hidden_size]),
+                                                 trainable=True, name="random_attn_vector")
+                pooled_question_rep = tc.layers.fully_connected(
+                    attend_pooling(question_vectors, random_attn_vector, self.hidden_size),
+                    num_outputs=self.hidden_size, activation_fn=None
+                )
+                init_state = tc.rnn.LSTMStateTuple(pooled_question_rep, pooled_question_rep)
+            else:
+                init_state = None
+            with tf.variable_scope('fw'):
+                fw_cell = PointerNetLSTMCell(self.hidden_size, passage_vectors)
+                fw_outputs, _ = custom_dynamic_rnn(fw_cell, fake_inputs, sequence_len, init_state)
+            with tf.variable_scope('bw'):
+                bw_cell = PointerNetLSTMCell(self.hidden_size, passage_vectors)
+                bw_outputs, _ = custom_dynamic_rnn(bw_cell, fake_inputs, sequence_len, init_state)
+            start_prob = (fw_outputs[0:, 0, 0:] + bw_outputs[0:, 1, 0:]) / 2
+            end_prob = (fw_outputs[0:, 1, 0:] + bw_outputs[0:, 0, 0:]) / 2
+            return start_prob, end_prob
+
+class MatchLSTMAttnCell(tc.rnn.LSTMCell):
+    """
+    Implements the Match-LSTM attention cell
+    """
+    def __init__(self, num_units, context_to_attend):
+        super(MatchLSTMAttnCell, self).__init__(num_units, state_is_tuple=True)
+        self.context_to_attend = context_to_attend
+        self.fc_context = tc.layers.fully_connected(self.context_to_attend,
+                                                    num_outputs=self._num_units,
+                                                    activation_fn=None)
+
+    def __call__(self, inputs, state, scope=None):
+        (c_prev, h_prev) = state
+        with tf.variable_scope(scope or type(self).__name__):
+            ref_vector = tf.concat([inputs, h_prev], -1)
+            G = tf.tanh(self.fc_context
+                        + tf.expand_dims(tc.layers.fully_connected(ref_vector,
+                                                                   num_outputs=self._num_units,
+                                                                   activation_fn=None), 1))
+            logits = tc.layers.fully_connected(G, num_outputs=1, activation_fn=None)
+            scores = tf.nn.softmax(logits, 1)
+            attended_context = tf.reduce_sum(self.context_to_attend * scores, axis=1)
+            new_inputs = tf.concat([inputs, attended_context,
+                                    inputs - attended_context, inputs * attended_context],
+                                   -1)
+            return super(MatchLSTMAttnCell, self).__call__(new_inputs, state, scope)
+
+
+class MatchLSTMLayer(object):
+    """
+    Implements the Match-LSTM layer, which attend to the question dynamically in a LSTM fashion.
+    """
+    def __init__(self, hidden_size):
+        self.hidden_size = hidden_size
+
+    def match(self, passage_encodes, question_encodes, p_length, q_length):
+        """
+        Match the passage_encodes with question_encodes using Match-LSTM algorithm
+        """
+        with tf.variable_scope('match_lstm'):
+            cell_fw = MatchLSTMAttnCell(self.hidden_size, question_encodes)
+            cell_bw = MatchLSTMAttnCell(self.hidden_size, question_encodes)
+            outputs, state = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw,
+                                                             inputs=passage_encodes,
+                                                             sequence_length=p_length,
+                                                             dtype=tf.float32)
+            match_outputs = tf.concat(outputs, 2)
+            state_fw, state_bw = state
+            c_fw, h_fw = state_fw
+            c_bw, h_bw = state_bw
+            match_state = tf.concat([h_fw, h_bw], 1)
+        return match_outputs, match_state
+
+
+class AttentionFlowMatchLayer(object):
+    """
+    Implements the Attention Flow layer,
+    which computes Context-to-question Attention and question-to-context Attention
+    """
+    def __init__(self, hidden_size):
+        self.hidden_size = hidden_size
+
+    def match(self, passage_encodes, question_encodes, p_length, q_length):
+        """
+        Match the passage_encodes with question_encodes using Attention Flow Match algorithm
+        """
+        with tf.variable_scope('bidaf'):
+            sim_matrix = tf.matmul(passage_encodes, question_encodes, transpose_b=True)
+            context2question_attn = tf.matmul(tf.nn.softmax(sim_matrix, -1), question_encodes)
+            b = tf.nn.softmax(tf.expand_dims(tf.reduce_max(sim_matrix, 2), 1), -1)
+            question2context_attn = tf.tile(tf.matmul(b, passage_encodes),
+                                         [1, tf.shape(passage_encodes)[1], 1])
+            concat_outputs = tf.concat([passage_encodes, context2question_attn,
+                                        passage_encodes * context2question_attn,
+                                        passage_encodes * question2context_attn], -1)
+            return concat_outputs, None
